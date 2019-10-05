be1599900d9aeec9461aece470d915e5cc6acd10
Sowraf <sowraf@gmail.com>
2019-10-05 12:08:52 +0530

    New project started

---
+++ Three.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body>
+<form method="post">
+Name: <input type="text" name="name"><br>
+House Name: <input type="text" name="hName"><br>
+<input type="submit">
+</form>
+</body>
+</html>
