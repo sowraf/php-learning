99bb9f75cc408e5e7ee09e06359a96586f542fd4
Sowraf Purushothaman <55350540+sowraf@users.noreply.github.com>
2019-10-05 13:10:47 +0530

    Update Three.php
    Added action for the form

--- Three.php
+++ Three.php
@@ -1,10 +1,30 @@
 <!DOCTYPE html>
 <html>
 <body>
-<form method="post">
+<form method="post" action="<?php echo $_SERVER['PHP_SELF']; ?>">
 Name: <input type="text" name="name"><br>
 House Name: <input type="text" name="hName"><br>
 <input type="submit">
 </form>
+  
+  <?php
+  if($_SERVER["REQUEST_METHOD"]=="POST"]
+     {
+       $name=$_POST["name"];
+       $hName=$_POST["hName"];
+       if(empty($name))
+          {
+            echo "No Input";
+       }
+       else
+       { echo "Name: ".$name; }
+       
+       if(empty($hName))
+          {
+            echo "No Input";
+       }
+       else
+       { echo "House Name: ".$hName; }
+  
 </body>
 </html>
