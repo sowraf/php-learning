b09a029704c108dbe03027c521f16cc4b2d03e0c
Sowraf <sowraf@gmail.com>
2019-10-30 00:31:50 +0530

    Formatting, removed unwanted spaces

--- register.php
+++ register.php
@@ -5,9 +5,9 @@ require_once 'config.php';
 $username = $password = $confirm_password = "";
 $username_err = $password_err = $confirm_password_err = "";
 
-if($_SERVER['REQUEST_METHOD'] == 'POST'){
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-    if(empty(trim($_POST['username']))){
+    if (empty(trim($_POST['username']))) {
 
         $username_err = "Username cannot be empty";
     } else {
@@ -16,15 +16,15 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
         $stmt = $conn->prepare($sql);
 
-        $stmt->bind_param('s',$param_username);
+        $stmt->bind_param('s', $param_username);
 
         $param_username = trim($_POST['username']);
 
-        if($stmt->execute()){
-            
+        if ($stmt->execute()) {
+
             $stmt->store_result();
 
-            if($stmt->num_rows == 1){
+            if ($stmt->num_rows == 1) {
 
                 $username_err = "Username already exists";
             } else {
@@ -35,13 +35,14 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
             echo "Something went wrong, please try again later";
         }
-        
+
         $stmt->close();
     }
 
-    if(empty(trim($_POST['password']))){
+    if (empty(trim($_POST['password']))) {
 
         $password_err = "Password field cannot be empty";
+    } elseif (strlen(trim($_POST['password'])) < 8) {
 
         $password_err = "Password must be atleast 8 characters long";
     } else {
@@ -49,33 +50,34 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         $password = trim($_POST['password']);
     }
 
-    if(empty(trim($_POST['confirm_password']))){
+    if (empty(trim($_POST['confirm_password']))) {
 
         $confirm_password_err = "Confirm password field cannot be empty";
     } else {
 
         $confirm_password = trim($_POST['confirm_password']);
-        if($password != $confirm_password){
+        if ($password != $confirm_password) {
 
-            $confirm_password_err = "Password mismatch";      
+            $confirm_password_err = "Password mismatch";
         }
     }
+
+    if (empty($username_err) && empty($password_err) && empty($confirm_password_err)) {
 
         $sql = 'INSERT INTO users (username,password) VALUES (?,?)';
 
         $stmt = $conn->prepare($sql);
 
-        $stmt->bind_param('ss',$username,$param_password);
+        $stmt->bind_param('ss', $username, $param_password);
 
-        $param_password = password_hash($password,PASSWORD_DEFAULT);
+        $param_password = password_hash($password, PASSWORD_DEFAULT);
 
-        if($stmt->execute()){
+        if ($stmt->execute()) {
 
             header("location: login.php");
         } else {
 
-            Echo "Something went wrong, Please try again later";
-
+            echo "Something went wrong, Please try again later";
         }
 
         $stmt->close();
@@ -88,24 +90,27 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
 <!DOCTYPE html>
 <html>
+
 <head>
 </head>
+
 <body>
 
-<h2> Sign Up </h2>
-<p> Fill in the details to sign up </p>
-<form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?> " method="POST" >
-<label> Username: </label> <br>
-<input type = "text" name="username"> <br>
-<span> <?php echo $username_err; ?> </span> <br>
-<label> Password: </label> <br>
-<input type = "password" name="password"> <br>
-<span> <?php echo $password_err; ?> </span> <br>
-<label> Confirm password: </label> <br>
-<input type = "password" name="confirm_password"> <br>
-<span> <?php echo $confirm_password_err; ?> </span> <br><br>
-<input type="submit" value="Submit">
-<input type="reset" value="Reset"> 
-<p> Already a member? <a href='login.php'> Login </a> </p>
-
+    <h2> Sign Up </h2>
+    <p> Fill in the details to sign up </p>
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?> " method="POST">
+        <label> Username: </label> <br>
+        <input type="text" name="username"> <br>
+        <span> <?php echo $username_err; ?> </span> <br>
+        <label> Password: </label> <br>
+        <input type="password" name="password"> <br>
+        <span> <?php echo $password_err; ?> </span> <br>
+        <label> Confirm password: </label> <br>
+        <input type="password" name="confirm_password"> <br>
+        <span> <?php echo $confirm_password_err; ?> </span> <br><br>
+        <input type="submit" value="Submit">
+        <input type="reset" value="Reset">
+        <p> Already a member? <a href='login.php'> Login </a> </p>
+    </form>
+</body>
 </html>
